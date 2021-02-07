d0e94f21a7af4039bb299ac0669dba0dad94fbcc
Angel Newton <xanewton@gmail.com>
2021-02-07 16:16:49 -0500

    Use ContentView_Previews to preview a hardcoded list of recipes

--- MyFavouriteRecipes/ContentView.swift
+++ MyFavouriteRecipes/ContentView.swift
@@ -8,25 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let recipeNames = ["Italian Pizza Chicken", "Greek Pasta Bake", "Hearty Parsnip Soup"]
-    @ObservedObject var recipeModel = RecipeModel()
-    
+    var recipeNames = [String]()
     var body: some View {
-         Group {
-             VStack {
-                 List(recipeNames, id: \.self) { name in
-                     Text("\(name)")
-                 }
-                 List(recipeModel.recipes, id: \.self) { name in
-                     Text("\(name)")
-                 }
-             }
-         }
-     }
+            VStack {
+                List(recipeNames, id: \.self) { name in
+                    Text("\(name)")
+                }
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(recipeNames: ["Italian Pizza Chicken", "Greek Pasta Bake", "Hearty Parsnip Soup"])
     }
 }
