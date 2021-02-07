9896df235d392f0ae55b7bff6a2bc9c3c44d104e
Angel Newton <xanewton@gmail.com>
2021-02-07 16:48:38 -0500

    Simplify RecipeModel.swift

--- MyFavouriteRecipes/ContentView.swift
+++ MyFavouriteRecipes/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    var recipeNames = [String]()
+    var recipes = [RecipeModel]()
     var body: some View {
-        VStack {
-            List(recipeNames, id: \.self) { name in
+        VStack(alignment: .leading) {
+            List(recipes, id: \.id) { recipe in
                 VStack {
-                    Text("\(name)")
+                    Text("\(recipe.name)")
                         .font(.headline)
-                    Text("\(name)")
+                    Text("\(recipe.origin)")
                         .font(.subheadline)
                 }
             }
@@ -25,6 +25,17 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(recipeNames: ["Italian Pizza Chicken", "Greek Pasta Bake", "Hearty Parsnip Soup"])
+        ContentView(recipes: ContentPreviewHelper.mockRecipes())
+    }
+}
+
+struct ContentPreviewHelper {
+    static func mockRecipes() -> [RecipeModel] {
+        var recipes = [RecipeModel]()
+        recipes.append(RecipeModel(id: UUID(), name: "Italian Pizza Chicken", origin: "Italian"))
+        recipes.append(RecipeModel(id: UUID(), name: "Greek Pasta Bake", origin: "Greek"))
+        recipes.append(RecipeModel(id: UUID(), name: "Hearty Parsnip Soup", origin: "British"))
+        recipes.append(RecipeModel(id: UUID(), name: "Honey & Soy Salmon", origin: "Chinese"))
+        return recipes
     }
 }
--- MyFavouriteRecipes/RecipeModel.swift
+++ MyFavouriteRecipes/RecipeModel.swift
@@ -7,17 +7,8 @@
 
 import Foundation
 
-// Simple model following the Observable pattern.
-class RecipeModel: Identifiable, ObservableObject {
-    @Published var recipes = [String]()
+struct RecipeModel: Identifiable, Hashable {
     var id = UUID()
-    
-    init() {
-        // Simulate calling an API by hardcoding some text.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.recipes.append(contentsOf: ["Pork & Potato Hotpot",
-                                              "Thai Green Curry",
-                                              "Italian Sausage & Beans"])
-        }
-    }
+    var name = ""
+    var origin = ""
 }
