dfce149398771ea881fa017a65968ae84f9bd9d8
Angel Newton <xanewton@gmail.com>
2021-02-08 18:07:07 -0500

    Add Navigation to show the RecipeDetailView from the list

--- MyFavouriteRecipes/ContentView.swift
+++ MyFavouriteRecipes/ContentView.swift
@@ -16,23 +16,30 @@ struct ContentView: View {
     @State private var viewIndex = 0
     
     var body: some View {
-        VStack(alignment: .leading) {
-            VStack {
-                Picker(selection: $viewIndex, label: Text("")) {
-                    Text("Recipes").tag(0)
-                    Text("Favourites").tag(1)
-                }.pickerStyle(SegmentedPickerStyle())
-                
-                if viewIndex == 0 {
-                    List(recipes, id: \.id) { recipe in
-                        RecipeView(recipe: recipe)
-                    }
-                } else if viewIndex == 1 {
-                    List(Helper.getFavourites(), id: \.id) { recipe in
-                        RecipeView(recipe: recipe)
+        NavigationView {
+            VStack(alignment: .leading) {
+                VStack {
+                    Picker(selection: $viewIndex, label: Text("")) {
+                        Text("Recipes").tag(0)
+                        Text("Favourites").tag(1)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    if viewIndex == 0 {
+                        List(recipes, id: \.id) { recipe in
+                            NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                                RecipeView(recipe: recipe)
+                            }
+                        }
+                    } else if viewIndex == 1 {
+                        List(Helper.getFavourites(), id: \.id) { recipe in
+                            NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                                RecipeView(recipe: recipe)
+                            }
+                        }
                     }
                 }
             }
+            .navigationBarTitle(Text("My Favourite Recipes"))
         }
     }
 }
