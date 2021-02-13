bf22c6f7cf549f45c96af8b070b8d14b0b7fdade
Angel Newton <xanewton@gmail.com>
2021-02-13 17:07:11 -0500

    Try to receive data on WatchOS. Not working

---
+++ MyFavoriteRecipesWatch Extension/ViewModelWatch.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModelWatch.swift
+//  MyFavoriteRecipesWatch Extension
+//
+//  Created by newtan on 2021-02-13.
+//
+
+import Foundation
---
+++ MyFavoriteRecipesWatch Extension/WatchHelper.swift
@@ -0,0 +1,8 @@
+//
+//  WatchHelper.swift
+//  MyFavoriteRecipesWatch Extension
+//
+//  Created by newtan on 2021-02-13.
+//
+
+import Foundation
