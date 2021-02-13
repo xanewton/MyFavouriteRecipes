7ec2cec1933372913f71b8350cf5a30ff6358233
Angel Newton <xanewton@gmail.com>
2021-02-13 18:15:33 -0500

    Try to receive data on WatchOS

--- MyFavoriteRecipesWatch Extension/ViewModelWatch.swift
+++ MyFavoriteRecipesWatch Extension/ViewModelWatch.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import WatchConnectivity
+
+// Based on Article: https://medium.com/better-programming/get-started-with-watch-connectivity-with-swiftui-51722324b6f6
+class ViewModelWatch : NSObject,  WCSessionDelegate, ObservableObject{
+    
+    var session: WCSession
+    @Published var messageText = ""
+    
+    init(session: WCSession = .default) {
+        self.session = session
+        super.init()
+        self.session.delegate = self
+        session.activate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+        DispatchQueue.main.async {
+            self.messageText = message["message"] as? String ?? "Unknown"
+        }
+    }
+    
+}
--- MyFavoriteRecipesWatch Extension/WatchHelper.swift
+++ MyFavoriteRecipesWatch Extension/WatchHelper.swift
@@ -5,4 +5,30 @@
 //  Created by newtan on 2021-02-13.
 //
 
-import Foundation
+import UIKit
+
+class WatchHelper: Any {
+
+    // Add Recipe
+    static func addRecipe(recipeString: String) -> [RecipeModel] {
+        var recipes = getRecipes()
+        let recipe = RecipeModel.createFrom(json: recipeString)
+        recipes.append(recipe)
+        
+        do {
+            let data = try JSONEncoder().encode(recipes)
+            UserDefaults.standard.set(data, forKey: "recipes")
+        } catch {
+            print("Unable to persist recipe")
+        }
+        return getRecipes()
+    }
+    
+    static func getRecipes() -> [RecipeModel] {
+        if let data = UserDefaults.standard.data(forKey: "recipes") {
+            let array = try! JSONDecoder().decode([RecipeModel].self, from: data)
+            return array
+        }
+        return [RecipeModel]()
+    }
+}
