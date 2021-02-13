d7f908fd7561a019443d247b1557f846e79a0d94
Angel Newton <xanewton@gmail.com>
2021-02-13 14:56:03 -0500

    Add WatchOS application to proyect

---
+++ MyFavoriteRecipesWatch Extension/MyFavouriteRecipesApp.swift
@@ -0,0 +1,19 @@
+//
+//  MyFavouriteRecipesApp.swift
+//  MyFavoriteRecipesWatch Extension
+//
+//  Created by newtan on 2021-02-13.
+//
+
+import SwiftUI
+
+@main
+struct MyFavouriteRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
